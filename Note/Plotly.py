ba33d4375ffcc6d8cd6c049d3ba6f963be83cc37
Chunibyo <359806334@qq.com>
2019-01-23 17:42:36 +0800

    19/01/23

--- Note/Plotly.py
+++ Note/Plotly.py
@@ -4,17 +4,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import plotly.plotly as py
+import plotly
 from plotly import tools
+import plotly.plotly as py
 import plotly.graph_objs as go
 
-# tools.set_credentials_file(username='chunibyo.wly', api_key='kjcebxiY2SZfb2KyPbsX')
-#
-# df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/school_earnings.csv")
-#
-# data = [go.Bar(x=df.School, y=df.Gap)]
+tools.set_credentials_file(username='chunibyo.wly', api_key='kjcebxiY2SZfb2KyPbsX')
 
-# py.plot(data)
+# https://www.cnblogs.com/feffery/p/9293745.html
+
 trace0 = go.Scatter(
     x=[1, 2, 3, 4],
     y=[10, 15, 13, 17]
@@ -23,6 +21,37 @@ trace1 = go.Scatter(
     x=[1, 2, 3, 4],
     y=[16, 5, 11, 9]
 )
-data = go.Data([trace0, trace1])
 
-py.plot(data, filename='basic-line')
+'''创建仿真数据'''
+N = 100
+random_x = np.linspace(0, 1, N)
+random_y0 = np.random.randn(N) + 5
+random_y1 = np.random.randn(N)
+random_y2 = np.random.randn(N) - 5
+
+'''构造trace0'''
+trace2 = go.Scatter(
+    x=random_x,
+    y=random_y0,
+    mode='markers',
+    name='markers'
+)
+
+'''构造trace1'''
+trace3 = go.Bar(
+    x=random_x,
+    y=random_y1,
+    # mode='lines+markers',
+    # name='lines+markers'
+)
+
+'''构造trace2'''
+trace4 = go.Scatter(
+    x=random_x,
+    y=random_y2,
+    mode='lines',
+    name='lines'
+)
+
+data = [trace0, trace1, trace2, trace3, trace4]
+py.plot(data, filename='basic-line.html')
---
+++ Note/Stock Market Analysis.py
@@ -0,0 +1,19 @@
+# Let's go ahead and start with some imports
+import pandas as pd
+from pandas import Series, DataFrame
+import numpy as np
+
+# For Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style('whitegrid')
+
+# For reading stock data from yahoo
+from pandas.io.data import DataReader
+
+# For time stamps
+from datetime import datetime
+
+# For division
+from __future__ import division
