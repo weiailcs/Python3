876bfc96de417cc9222196de3da296b5ebe18000
Chunibyo <359806334@qq.com>
2019-02-03 19:39:30 +0800

    19/02/03

--- GraphCut/Main.py
+++ GraphCut/Main.py
@@ -1,24 +1,17 @@
 # -*- coding: utf-8 -*-
 
-# import numpy as np
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import cv2
-#
-# img = cv2.imread('data.png', 0)
-# n, m = img.shape
-# # (981, 1697, 3)
-# print(img.dtype)
-# # uint8
-# # for i in range(25):
-# #     for j in range(25):
-# #         print(img[i][j], end=' ')
-# #         # print(' ')
-# #     print('\n')
-# cv2.imshow("GrayScale", img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-# s = np.random.seed(13331)
-# a = np.random.randint(3, 10, size=40)
-# np.savetxt('data2.txt', a)
-print(1)
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import cv2
+
+img = cv2.imread('data.png', 0)
+n, m = img.shape
+# (981, 1697, 3)
+print(img.dtype)
+# uint8
+
+cv2.imshow("GrayScale", img)
+cv2.imwrite('output.png', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
