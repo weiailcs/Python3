9b62ed2de027b9c585046b078c613c99cd615393
chunibyo-wly <chuniyo.wly@gmail.com>
2019-05-11 16:15:11 +0800

    19/05/11

---
+++ crawler/test.py
@@ -0,0 +1,9 @@
+import urllib
+# import urllib2
+# import cookielib
+import re
+import string
+
+login_url = 'http://sfrz.cug.edu.cn/tpass/login;jsessionid=EUmlngQE9X22n_VEeINfqRae15mtD8fjxNNW5FvUqbZpMxWQeo4h' \
+            '!952804022?service=http%3A%2F%2Fxyfw.cug.edu.cn%2Ftp_up%2F '
+
